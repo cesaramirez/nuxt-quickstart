16179622b2479b3452cfcef45cc8c456e39c0b1f
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-03-03 12:51:59 -0600

    :sparkles: Add axios and make request from fake API

---
+++ pages/posts/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>Posts</h3>
+    <pre>
+      <code>
+        {{ posts }}
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async asyncData() {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+    return { posts: data }
+  },
+  mounted() {
+    this.posts
+  }
+}
+</script>
