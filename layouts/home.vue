51cf2112729f3e7bf89ff99202b532fe75507650
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-03-03 19:45:47 -0600

    :sparkles: Add custom home page

---
+++ layouts/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center h-screen w-screen bg-black">
+    <nuxt/>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <h3 class="text-xl">Home</h3>
+  <div class="text-center">
+    <h3 class="text-4xl text-white mb-10">Home</h3>
+    <nuxt-link
+      :to="{ name: 'posts' }"
+      class="text-black bg-white px-8 py-4 border rounded-full no-underline uppercase font-bold hover:bg-grey-light">Go to Posts</nuxt-link>
   </div>
 </template>
 
 <script>
 
 export default {
-
+  layout: 'home'
 }
 </script>
